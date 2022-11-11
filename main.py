ac44e9a1a91aa1bfc59bc4f29a21a4d8440ce0f1
Zsófia Győri <zsofia.gyori00@gmail.com>
2022-11-11 19:56:02 +0100

    create and authorize bot & test if it works

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+#Imports
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
 
-print("LESS GOO")
+# Create bot
+client = commands.Bot(intents=discord.Intents.all() , command_prefix= "!")
+
+# Startup Information
+@client.event
+async def on_ready():
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
+
+# Command
+@client.command()
+async def helloworld(ctx):
+    await ctx.send('Hello World!')
+
+#Get token from .env file
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+#Run the bot
+client.run(TOKEN)
+
