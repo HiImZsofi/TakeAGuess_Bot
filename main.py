10dea1a53543f39fb77ed7e349b20079d535b442
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-11-14 15:14:42 +0100

    Make some variables global

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ gmaps = googlemaps.Client(key=MAPS_TOKEN)
 client = commands.Bot(intents=discord.Intents.all() , command_prefix= "!")
 xdecimal = 0
 ydecimal = 0
+country=''
 
 # Startup Information
 @client.event
@@ -46,15 +47,15 @@ async def startgame(ctx):
     await ctx.send('First round\n'+formatLink())
 
 def generateXCoordinate():
+    global xdecimal
     x = random.uniform(-90.0000000, 90.000000)
     xdecimal = round(x, 6)
-    print(xdecimal)
     return xdecimal
 
 def generateYCoordinate():
+    global ydecimal
     y = random.uniform(-180.000000, 180.000000)
     ydecimal = round(y, 6)
-    print(ydecimal)
     return ydecimal
  
 #Function to replace longitude and latitude in link
@@ -63,6 +64,7 @@ def formatLink():
     reverse_geocode_result = gmaps.reverse_geocode((generateXCoordinate(), generateYCoordinate()))
 
     while(reverse_geocode_result[len(reverse_geocode_result)-1]['address_components'][len(reverse_geocode_result[len(reverse_geocode_result)-1]['address_components'])-1]['types'][0] == 'country'):
+        global country
         xdecimal = generateXCoordinate()
         ydecimal=generateYCoordinate()
         country = getCountry(xdecimal,ydecimal)
